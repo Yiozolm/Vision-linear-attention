11973f6b47fce048f0191c3458f07c9a24be9d91
BoyceYi <1473416941@qq.com>
2025-07-17 16:22:43 +0800

    fix deprecated parameters

--- vla/Blocks/GroupMamba/csms6s.py
+++ vla/Blocks/GroupMamba/csms6s.py
@@ -7,6 +7,7 @@
 # --------------------------------------------------------
 import torch
 
+
 # original scans =============
 class CrossScan(torch.autograd.Function):
     @staticmethod
@@ -18,7 +19,7 @@ class CrossScan(torch.autograd.Function):
         xs[:, 1] = x.transpose(dim0=2, dim1=3).flatten(2, 3)
         xs[:, 2:4] = torch.flip(xs[:, 0:2], dims=[-1])
         return xs
-    
+
     @staticmethod
     def backward(ctx, ys: torch.Tensor):
         # out: (b, k, d, l)
@@ -38,7 +39,7 @@ class CrossMerge(torch.autograd.Function):
         ys = ys[:, 0:2] + ys[:, 2:4].flip(dims=[-1]).view(B, 2, D, -1)
         y = ys[:, 0] + ys[:, 1].view(B, -1, W, H).transpose(dim0=2, dim1=3).contiguous().view(B, D, -1)
         return y
-    
+
     @staticmethod
     def backward(ctx, x: torch.Tensor):
         # B, D, L = x.shape
@@ -52,6 +53,7 @@ class CrossMerge(torch.autograd.Function):
         xs = xs.view(B, 4, C, H, W)
         return xs
 
+
 # partial scan 1 =============
 class CrossScan_1(torch.autograd.Function):
     @staticmethod
@@ -61,7 +63,7 @@ class CrossScan_1(torch.autograd.Function):
         xs = x.new_empty((B, 1, C, H * W))
         xs[:, 0] = x.flatten(2, 3)
         return xs
-    
+
     @staticmethod
     def backward(ctx, ys: torch.Tensor):
         # out: (b, k, d, l)
@@ -69,6 +71,7 @@ class CrossScan_1(torch.autograd.Function):
         L = H * W
         y = ys[:, 0]
         return y.view(B, -1, H, W)
+
 
 class CrossMerge_1(torch.autograd.Function):
     @staticmethod
@@ -78,7 +81,7 @@ class CrossMerge_1(torch.autograd.Function):
         ys = ys.view(B, K, D, -1)
         y = ys[:, 0]
         return y
-    
+
     @staticmethod
     def backward(ctx, x: torch.Tensor):
         # B, D, L = x.shape
@@ -90,6 +93,7 @@ class CrossMerge_1(torch.autograd.Function):
         xs = xs.view(B, 1, C, H, W)
         return xs
 
+
 # partial scan 2 =============
 class CrossScan_2(torch.autograd.Function):
     @staticmethod
@@ -99,7 +103,7 @@ class CrossScan_2(torch.autograd.Function):
         xs = x.new_empty((B, 1, C, H * W))
         xs[:, 0] = x.transpose(dim0=2, dim1=3).flatten(2, 3)
         return xs
-    
+
     @staticmethod
     def backward(ctx, ys: torch.Tensor):
         # out: (b, k, d, l)
@@ -107,6 +111,7 @@ class CrossScan_2(torch.autograd.Function):
         L = H * W
         y = ys[:, 0].view(B, -1, H, W).transpose(dim0=2, dim1=3).contiguous().view(B, -1, L)
         return y.view(B, -1, H, W)
+
 
 class CrossMerge_2(torch.autograd.Function):
     @staticmethod
@@ -116,7 +121,7 @@ class CrossMerge_2(torch.autograd.Function):
         ys = ys.view(B, K, D, -1)
         y = ys[:, 0].view(B, -1, W, H).transpose(dim0=2, dim1=3).contiguous().view(B, D, -1)
         return y
-    
+
     @staticmethod
     def backward(ctx, x: torch.Tensor):
         # B, D, L = x.shape
@@ -128,6 +133,7 @@ class CrossMerge_2(torch.autograd.Function):
         xs = xs.view(B, 1, C, H, W)
         return xs
 
+
 # partial scan 3 =============
 
 class CrossScan_3(torch.autograd.Function):
@@ -138,7 +144,7 @@ class CrossScan_3(torch.autograd.Function):
         xs = x.new_empty((B, 1, C, H * W))
         xs[:, 0] = torch.flip(x.flatten(2, 3), dims=[-1])
         return xs
-    
+
     @staticmethod
     def backward(ctx, ys: torch.Tensor):
         # out: (b, k, d, l)
@@ -146,6 +152,7 @@ class CrossScan_3(torch.autograd.Function):
         L = H * W
         y = ys[:, 0].flip(dims=[-1]).view(B, 1, -1, L)
         return y.view(B, -1, H, W)
+
 
 class CrossMerge_3(torch.autograd.Function):
     @staticmethod
@@ -155,7 +162,7 @@ class CrossMerge_3(torch.autograd.Function):
         ys = ys.view(B, K, D, -1)
         y = ys[:, 0].flip(dims=[-1]).view(B, D, -1)
         return y
-    
+
     @staticmethod
     def backward(ctx, x: torch.Tensor):
         # B, D, L = x.shape
@@ -167,6 +174,7 @@ class CrossMerge_3(torch.autograd.Function):
         xs = xs.view(B, 1, C, H, W)
         return xs
 
+
 # partial scan 4 =============
 class CrossScan_4(torch.autograd.Function):
     @staticmethod
@@ -176,14 +184,16 @@ class CrossScan_4(torch.autograd.Function):
         xs = x.new_empty((B, 1, C, H * W))
         xs[:, 0] = torch.flip(x.transpose(dim0=2, dim1=3).flatten(2, 3), dims=[-1])
         return xs
-    
+
     @staticmethod
     def backward(ctx, ys: torch.Tensor):
         # out: (b, k, d, l)
         B, C, H, W = ctx.shape
         L = H * W
-        y = ys[:, 0].view(B, -1, H, W).transpose(dim0=2, dim1=3).contiguous().view(B, -1, L).flip(dims=[-1]).view(B, 1, -1, L)
+        y = ys[:, 0].view(B, -1, H, W).transpose(dim0=2, dim1=3).contiguous().view(B, -1, L).flip(dims=[-1]).view(B, 1,
+                                                                                                                  -1, L)
         return y.view(B, -1, H, W)
+
 
 class CrossMerge_4(torch.autograd.Function):
     @staticmethod
@@ -193,7 +203,7 @@ class CrossMerge_4(torch.autograd.Function):
         ys = ys.view(B, K, D, -1)
         y = ys[:, 0].view(B, -1, W, H).transpose(dim0=2, dim1=3).contiguous().view(B, D, -1).flip(dims=[-1])
         return y
-    
+
     @staticmethod
     def backward(ctx, x: torch.Tensor):
         # B, D, L = x.shape
@@ -204,6 +214,7 @@ class CrossMerge_4(torch.autograd.Function):
         xs[:, 0] = torch.flip(x.view(B, C, H, W).transpose(dim0=2, dim1=3).flatten(2, 3), dims=[-1])
         xs = xs.view(B, 1, C, H, W)
         return xs
+
 
 # import selective scan ==============================
 try:
@@ -232,7 +243,8 @@ def check_nan_inf(tag: str, x: torch.Tensor, enable=True):
     if enable:
         if torch.isinf(x).any() or torch.isnan(x).any():
             print(tag, torch.isinf(x).any(), torch.isnan(x).any(), flush=True)
-            import pdb; pdb.set_trace()
+            import pdb;
+            pdb.set_trace()
 
 
 # fvcore flops =======================================
@@ -246,18 +258,19 @@ def flops_selective_scan_fn(B=1, L=256, D=768, N=16, with_D=True, with_Z=False, 
     D: r(D)
     z: r(B D L)
     delta_bias: r(D), fp32
-    
+
     ignores:
-        [.float(), +, .softplus, .shape, new_zeros, repeat, stack, to(dtype), silu] 
+        [.float(), +, .softplus, .shape, new_zeros, repeat, stack, to(dtype), silu]
     """
-    assert not with_complex 
+    assert not with_complex
     # https://github.com/state-spaces/mamba/issues/110
     flops = 9 * B * L * D * N
     if with_D:
         flops += B * D * L
     if with_Z:
-        flops += B * D * L    
+        flops += B * D * L
     return flops
+
 
 # this is only for selective_scan_ref...
 def flops_selective_scan_ref(B=1, L=256, D=768, N=16, with_D=True, with_Z=False, with_Group=True, with_complex=False):
@@ -270,12 +283,12 @@ def flops_selective_scan_ref(B=1, L=256, D=768, N=16, with_D=True, with_Z=False,
     D: r(D)
     z: r(B D L)
     delta_bias: r(D), fp32
-    
+
     ignores:
-        [.float(), +, .softplus, .shape, new_zeros, repeat, stack, to(dtype), silu] 
+        [.float(), +, .softplus, .shape, new_zeros, repeat, stack, to(dtype), silu]
     """
     import numpy as np
-    
+
     # fvcore.nn.jit_handles
     def get_flops_einsum(input_shapes, equation):
         np_arrs = [np.zeros(s) for s in input_shapes]
@@ -285,58 +298,59 @@ def flops_selective_scan_ref(B=1, L=256, D=768, N=16, with_D=True, with_Z=False,
                 # divided by 2 because we count MAC (multiply-add counted as one flop)
                 flop = float(np.floor(float(line.split(":")[-1]) / 2))
                 return flop
-    
 
     assert not with_complex
 
-    flops = 0 # below code flops = 0
+    flops = 0  # below code flops = 0
 
     flops += get_flops_einsum([[B, D, L], [D, N]], "bdl,dn->bdln")
     if with_Group:
         flops += get_flops_einsum([[B, D, L], [B, N, L], [B, D, L]], "bdl,bnl,bdl->bdln")
     else:
         flops += get_flops_einsum([[B, D, L], [B, D, N, L], [B, D, L]], "bdl,bdnl,bdl->bdln")
-  
-    in_for_flops = B * D * N   
+
+    in_for_flops = B * D * N
     if with_Group:
         in_for_flops += get_flops_einsum([[B, D, N], [B, D, N]], "bdn,bdn->bd")
     else:
         in_for_flops += get_flops_einsum([[B, D, N], [B, N]], "bdn,bn->bd")
-    flops += L * in_for_flops 
+    flops += L * in_for_flops
     if with_D:
         flops += B * D * L
     if with_Z:
-        flops += B * D * L  
+        flops += B * D * L
     return flops
 
 
 def print_jit_input_names(inputs):
     print("input params: ", end=" ", flush=True)
-    try: 
+    try:
         for i in range(10):
             print(inputs[i].debugName(), end=" ", flush=True)
     except Exception as e:
         pass
     print("", flush=True)
 
+
 # cross selective scan ===============================
 # comment all checks if inside cross_selective_scan
 class SelectiveScanMamba(torch.autograd.Function):
     @staticmethod
-    @torch.cuda.amp.custom_fwd
-    def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1, oflex=True):
+    @torch.amp.custom_fwd(device_type='cuda')
+    def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1,
+                oflex=True):
         ctx.delta_softplus = delta_softplus
         out, x, *rest = selective_scan_cuda.fwd(u, delta, A, B, C, D, None, delta_bias, delta_softplus)
         ctx.save_for_backward(u, delta, A, B, C, D, delta_bias, x)
         return out
-    
+
     @staticmethod
-    @torch.cuda.amp.custom_bwd
+    @torch.amp.custom_fwd(device_type='cuda')
     def backward(ctx, dout, *args):
         u, delta, A, B, C, D, delta_bias, x = ctx.saved_tensors
         if dout.stride(-1) != 1:
             dout = dout.contiguous()
-        
+
         du, ddelta, dA, dB, dC, dD, ddelta_bias, *rest = selective_scan_cuda.bwd(
             u, delta, A, B, C, D, None, delta_bias, dout, x, None, None, ctx.delta_softplus,
             False
@@ -346,15 +360,16 @@ class SelectiveScanMamba(torch.autograd.Function):
 
 class SelectiveScanCore(torch.autograd.Function):
     @staticmethod
-    @torch.cuda.amp.custom_fwd
-    def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1, oflex=True):
+    @torch.amp.custom_fwd(device_type='cuda')
+    def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1,
+                oflex=True):
         ctx.delta_softplus = delta_softplus
         out, x, *rest = selective_scan_cuda_core.fwd(u, delta, A, B, C, D, delta_bias, delta_softplus, 1)
         ctx.save_for_backward(u, delta, A, B, C, D, delta_bias, x)
         return out
-    
+
     @staticmethod
-    @torch.cuda.amp.custom_bwd
+    @torch.amp.custom_bwd(device_type='cuda')
     def backward(ctx, dout, *args):
         u, delta, A, B, C, D, delta_bias, x = ctx.saved_tensors
         if dout.stride(-1) != 1:
@@ -367,15 +382,16 @@ class SelectiveScanCore(torch.autograd.Function):
 
 class SelectiveScanOflex(torch.autograd.Function):
     @staticmethod
-    @torch.cuda.amp.custom_fwd
-    def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1, oflex=True):
+    @torch.amp.custom_fwd(device_type='cuda')
+    def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1,
+                oflex=True):
         ctx.delta_softplus = delta_softplus
         out, x, *rest = selective_scan_cuda_oflex.fwd(u, delta, A, B, C, D, delta_bias, delta_softplus, 1, oflex)
         ctx.save_for_backward(u, delta, A, B, C, D, delta_bias, x)
         return out
-    
+
     @staticmethod
-    @torch.cuda.amp.custom_bwd
+    @torch.amp.custom_bwd(device_type='cuda')
     def backward(ctx, dout, *args):
         u, delta, A, B, C, D, delta_bias, x = ctx.saved_tensors
         if dout.stride(-1) != 1:
