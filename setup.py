b72fe2d8db52d2ad98aca1998c5b9674e16867fb
BoyceYi <1473416941@qq.com>
2025-07-22 16:24:15 +0800

    Buliding whole lib with Trae

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
 from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text(encoding='utf-8')
 
 setup(
-    name="vla",
-    version="0.1",
+    name="vision-linear-attention",
+    version="0.1.0",
     packages=find_packages(),
+    author="Fangzhou Yi",
+    author_email="m202310581@xs.ustb.edu.cn",
+    description="A collection of vision linear attention models",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yiozolm/Vision-linear-attention",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+    ],
+    install_requires=[
+        "torch>=2.5.1",
+        "ninja",
+        "timm",
+        "einops",
+        # Note: mamba-ssm requires manual installation from .whl files
+    ],
+    python_requires=">=3.11",
 )
