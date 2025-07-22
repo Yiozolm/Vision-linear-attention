1f4d117de39b0a75858a5faf8fb53fe5ed5db632
BoyceYi <1473416941@qq.com>
2025-07-22 15:48:28 +0800

    test more for interface

--- tests/test-base.py
+++ tests/test-base.py
@@ -1,5 +1,5 @@
 from vla.Blocks.Base import BasicBlock
-from fla.layers.comba import Comba
+from fla.layers.multiscale_retention import MultiScaleRetention
 from torchsummary import summary
 
 import warnings
@@ -9,23 +9,30 @@ if __name__ == "__main__":
     model = BasicBlock(
         dim=64,
         hidden_rate=4,
-        SpatialMixer=Comba
+        SpatialMixer=MultiScaleRetention
     ).cuda()
 
     summary(model, (64, 128, 128), device='cuda')
 
 '''
 Write your own summary
+- [ ]   ABC **triton.compiler.errors.CompilationError: at 26:14:**
+- [ ]   Based
+- [ ]   Comba **triton.runtime.errors.OutOfResources: out of resource: shared memory, Required: 147456, Hardware limit: 101376. Reducing block sizes or `num_stages` may help.**
+- [x]   DeltaNet **change your model & data to bfloat16**
+- [x]   DeltaProduct **change your model & data to bfloat16**
 - [x]   Fox
-- [ ]   DeltaNet **fp32**
+- [x]   GLA
+- [x]   GSA **slow**
 - [x]   HGRN
-- [ ]   HGRN2 **numhead_param**
-- [ ]   lightnet **numhead_param**
-- [x]   Gsa **slow**
+- [x]   HGRN2 **change your input channel to 128$\cdot$k **
+- [x]   Lightnet **numhead_param**
 - [x]   Mamba
 - [x]   MesaNet
+- [ ]   PathAttn
+- [ ]   ReBased
+- [x]   RetNet **Crazy digits**
 - [x]   Rodimus **chunk_gla Problem**
 - [x]   Rwkv6 
 - [ ]   Rwkv7 need v_first
-- [ ]   Comba **IndexError: list index out of range**
 '''
